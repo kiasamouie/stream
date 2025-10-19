cb4eaa5c76adeb6bc3dda00f90e1c11da35ed4c0
Ary Samouie <arysamouie@gmail.com>
2025-10-19 13:27:03 +0100

    Changes to ffmpeg parameters

--- svc_stream.sh
+++ svc_stream.sh
@@ -7,7 +7,6 @@ set +a
 
 echo "[svc_stream] $(date -Is) starting"
 
-# Wait for ALSA Loopback device
 until arecord -l | grep -q "Loopback"; do
   echo "[svc_stream] Waiting for ALSA Loopback device..."
   sleep 2
@@ -23,12 +22,12 @@ fi
 
 ffmpeg -hide_banner -loglevel info \
   -re -stream_loop -1 -thread_queue_size 2048 -i "$BG_FILE" \
-  -re -thread_queue_size 1024 -framerate 1 -loop 1 -i "$ARTWORK_URL" \
+  -re -thread_queue_size 1024 -framerate 0.5 -loop 1 -i "$ARTWORK_URL" \
   -thread_queue_size 65536 -f alsa -itsoffset 0.1 -ar 48000 -i "${ALSA_CAPTURE}" \
   -filter_complex "\
 [0:v]scale=${STREAM_WIDTH}:${STREAM_HEIGHT},setsar=1[v0]; \
-[v0][1:v]overlay=10:10:format=auto, \
-drawtext=fontfile=${FONT_PATH}:textfile=${TEXT_FILE}:reload=1:fontsize=24:fontcolor=white:shadowx=2:shadowy=2:x=120:y=15[outv]" \
+[v0][1:v]overlay=10:10:format=auto:eval=frame, \
+drawtext=fontfile=${FONT_PATH}:textfile=${TEXT_FILE}:reload=1:reload_interval=2:fontsize=24:fontcolor=white:shadowx=2:shadowy=2:x=120:y=15[outv]" \
   -map "[outv]" -map 2:a \
   -c:v libx264 -pix_fmt yuv420p -preset $X264_PRESET -b:v $VIDEO_BR \
   -x264-params keyint=$GOP:min-keyint=$FPS:scenecut=0 \
